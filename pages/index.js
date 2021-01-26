45539da224356d45e01ebee5ebf697573cd831b8
kevenescovedo <kevenescovedo1916@gmail.com>
2021-01-26 15:43:18 -0300

    api versão 2.0

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
  function Home() {
-    return <h1>nada aqui por enquanto fique ligado !!!!!</h1>;
+    return <div>
+    <h1>Pagina Apenas para ilustrar as rotas mais, ainda ele será estilizada</h1>
+    <p>para acessar dados de covid globais acesse rota utilizando o metodo get: https://api-covid-br.vercel.app/api/global</p>
+    <p>para acessar dados de covid no brasil acesse rota utilizando o metodo get: https://api-covid-br.vercel.app/api/brazil</p>
+    </div>;
 }
 export default Home
